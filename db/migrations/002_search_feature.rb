badd7d9cab7553503a939be8b0ec853ad0cb92dd
Yuan Zhuang <whywhy36@gmail.com>
2015-08-25 21:19:44 +1200

    add migration: postgres search in text

---
+++ db/migrations/002_search_feature.rb
@@ -0,0 +1,26 @@
+# quick SQL search example:
+# select name from ( select * from torrents, plainto_tsquery('nirvana') as q where (tsv @@ q)) as t1;
+
+Sequel.migration do
+  up do
+    run 'alter table torrents add column tsv tsvector;'
+    run 'create index tsv_index on torrents using gin(tsv);'
+    run "update torrents set tsv = to_tsvector(coalesce(name, ''));"
+    run p %{
+create function torrents_search_trigger() RETURNS trigger AS $$
+	begin
+		new.tsv := to_tsvector(coalesce(text, ''));
+		return new;
+	end
+$$ LANGUAGE plpgsql;
+    }
+    run 'create trigger tsvectorupdate before insert or update on torrents for each row execute procedure torrents_search_trigger();'
+  end
+
+  down do
+    run 'drop trigger if exists tsvectorupdate on torrents;'
+    run 'drop function if exists torrents_search_trigger();'
+    run 'drop index tsv_index'
+    drop_column :torrents, :tsv
+  end
+end
--- torrent_process/readme.md
+++ torrent_process/readme.md
@@ -1,1 +1,2 @@
 ##Python project (to use libtorrent Python binding).
+
